911b235bbbc4ac53ab8985cfe9a683b0999096f0
Amr <bawzeeramr@gmail.com>
2024-01-20 21:02:04 +0000

    Create basic operator functions

---
+++ app.js
@@ -0,0 +1,68 @@
+// let addBtn = document.querySelector('#add');
+// let subtractBtn = document.querySelector('#subtract');
+// let multiplyBtn = document.querySelector('#multiply');
+// let divideBtn = document.querySelector('#divide');
+
+// addBtn.addEventListener('click', () => {
+//     let firstNumber = prompt('enter first number');
+//     let secondNumber = prompt('enter second number');
+//     add(firstNumber, secondNumber);
+// });
+
+// subtractBtn.addEventListener('click', () => {
+//     let firstNumber = prompt('enter first number');
+//     let secondNumber = prompt('enter second number');
+//     subtract(firstNumber, secondNumber);
+// });
+
+// multiplyBtn.addEventListener('click', () => {
+//     let firstNumber = prompt('enter first number');
+//     let secondNumber = prompt('enter second number');
+//     multiply(firstNumber, secondNumber);
+// });
+
+// divideBtn.addEventListener('click', () => {
+//     let firstNumber = prompt('enter first number');
+//     let secondNumber = prompt('enter second number');
+//     divide(firstNumber, secondNumber);
+// });
+
+let operandOne = prompt('enter your first number');
+let operator = prompt('enter your operator +-*/');
+let operandTwo = prompt('enter your second number');
+
+function add(firstNumber, secondNumber){
+    console.log(+firstNumber + +secondNumber);
+};
+
+function subtract(firstNumber, secondNumber){
+    console.log(+firstNumber - +secondNumber);
+};
+
+function multiply(firstNumber, secondNumber){
+    console.log(+firstNumber * +secondNumber);
+};
+
+function divide(firstNumber, secondNumber){
+    console.log(+firstNumber / +secondNumber);
+};
+
+
+function operate(operandOne, operandTwo, operator) {
+    switch(operator) {
+        case '+':
+        add(operandOne, operandTwo);
+        break;
+        case '-':
+        subtract(operandOne, operandTwo);
+        break;
+        case '*':
+        multiply(operandOne, operandTwo);
+        break;
+        case '/':
+        divide(operandOne, operandTwo);
+        break;
+    }
+};
+
+operate(operandOne, operandTwo, operator);
